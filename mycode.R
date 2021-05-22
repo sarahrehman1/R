d59d0e7d8f954acecb62be9f0ac124e90c9f2743
hartikha <67902930+hartikha@users.noreply.github.com>
2021-05-22 22:09:18 +0100

    added code to plot points on bioclim and extract data

--- mycode.R
+++ mycode.R
@@ -93,8 +93,50 @@ plot(coord)
 plot(britain)
 plot(britain, add=T)
 
-?projectRaster
+# Plotting bryo data points on bioclim maps -------------------------------
+
+##HH there appears to be some issues with the datum, it is discarded - and throws a warning. 
+##the vignette below seems to suggest its because of an updates in the packahe.
+##https://cran.r-project.org/web/packages/rgdal/vignettes/PROJ6_GDAL3.html#Status_22_April_2020
+packageVersion("rgdal")
+rgdal_extSoftVersion()
+
+##it seems the +datum errors come even with other datums - therefore not specific to OSGB36!
+(crs <- CRS("+proj=longlat +ellps=WGS84")) #simply define a crs as a demonstrating
+## notice discarded datum warning. Document above says the error messages are over cautious
+
+discarded_datum <- showSRID("EPSG:27700", "PROJ") #this is the OSGB36 code
+discarded_datum # this is the outcome when trying to assign EPSG:27700
+# the projection seems to be OK despite the datum not defined. 
+str(coord) #notice the crs format here (str() just shows the object attributes)
+
+#trying to next plot these on to a bioclim map
+
+# taking bio2 and using Sarahs extent
+coordExt <- c(-12,	5, 49, 61) # UK extent 
+bio2crop <- crop(bio2, coordExt)
+plot(bio2crop)
+str(bio2crop)
+#notice this is a raster layer with crs "+proj=longlat +datum=WGS84 +no_defs"
+#reproject onto 27700 before laying the points on top
+
 # Changing projection
 # Use `projectRaster` function:
-bio19.swProj <- projectRaster(bio19.sw, crs="+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +units=m +no_defs")   # can also use a template raster, see ?projectRaster
-bio19.swProj   # notice info at coord.ref.
+bio2crop_sw <- projectRaster(bio2crop, crs=27700)
+plot(bio2crop_sw)
+str(bio2crop_sw) #note crs has changed and looks like the one for the coord file...
+#add the points
+plot(coord, add=T)
+
+#bonus - extract data from bio2crop_sw for coord points
+pts_data <- raster::extract(clim[[1:4]],pts,df=T)
+pts_data <- raster::extract(bio2crop_sw,coord,df=T)
+pts_data
+
+#and for all vars
+all_bioclimvars <- crop(bioclimVars, coordExt)
+all <- projectRaster(all_bioclimvars, crs=27700)
+all_pts_data <- raster::extract(all[[1:7]],coord,df=T)
+all_pts_data
+
+#over to sarah for triage
