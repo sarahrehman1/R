23e76a26b53666967e0768bd77fac25b48955692
sarahrehman1 <sarahrehman0@gmail.com>
2021-06-17 15:59:50 +0100

    here's my attempt at loading the adf files for human influence index

---
+++ loading adf.R
@@ -0,0 +1,13 @@
+getwd()
+HII <- raster("w001001x.adf")
+?raster
+require(raster)
+
+r = raster("C:/Users/nisar/Desktop/R_wd/R/w001001x.adf")
+
+
+read.adf("w001001.adf")
+plot(w001001x.adf)
+library(adfExplorer)
+library(rrd)
+install.packages("rrd")
