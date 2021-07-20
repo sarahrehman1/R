ae640c627e8ffc51800ead417d1e7f72ff219e2a
Hanna Hartikainen <Hanna.Hartikainen@nottingham.ac.uk>
2021-07-20 09:27:26 +0100

    added path to work on HH comp

--- loading adf.R
+++ loading adf.R
@@ -8,6 +8,7 @@ r = raster("C:/Users/nisar/Desktop/R_wd/R/w001001x.adf")
 
 #need to provide the whole folder for the import to work.
 HII_adf <- raster("C:/Users/hanna/switchdrive/04_RESEARCH_PROJECTS/02_RESEARCH_PROJECTS/R/hii_europe_geo_grid/hii_europe/w001001.adf") #on hannas comp
+HII_adf <- raster("C:/Users/mbzhh/switchdrive/04_RESEARCH_PROJECTS/02_RESEARCH_PROJECTS/R/hii_europe_geo_grid/hii_europe/w001001.adf") #on hannas comp
 plot(HII_adf)
 
 read.adf("w001001.adf")
